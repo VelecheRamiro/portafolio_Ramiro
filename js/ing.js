2a215d2b83ba6141ed328142e2251d732f85efce
Ramiro Veleche <45570012@intech.edu.ar>
2025-05-05 19:21:12 -0300

    js test

--- js/ing.js
+++ js/ing.js
@@ -3,6 +3,5 @@ let idiomaActaual = 'es;'
 const traducciones = { es:
 {}, en:{}};
 
-function cambir{} { idioma = idioma == 'es'?'en':'es';
-document
-}
+function cambir{} { idioma = idioma == 'es'?'en':'es';document}
+
